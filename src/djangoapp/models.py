ff300a4baa0c67a5b0b00fd97c884bbc2808b230
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-13 12:31:21 +0300

    Create model computer

--- src/djangoapp/models.py
+++ src/djangoapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Computer(models.Model):
+    computer_name = models.CharField(max_length=30)
+    IP_address = models.CharField(max_length=30)
+    MAC_address = models.CharField(max_length=30)
+    users_name = models.CharField(max_length=30)
+    location = models.CharField(max_length=30)
+
+def __unicode__(self):
+    return self.computer_name
