b470251de41399c2a8205fef85bc9037a36f641f
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-14 10:19:33 +0300

    Customise the front end

--- src/djangoapp/views.py
+++ src/djangoapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+	title = 'Welcome: This is the Home Page'
+	context = {
+	"title": title,
+	}
+	return render(request, "home.html",context)
--- src/djangoproject/urls.py
+++ src/djangoproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from djangoapp.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'), 
 ]
