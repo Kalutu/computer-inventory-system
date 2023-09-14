073b8e1c903475f75e6b5e5f3aeb51bf92cda5d5
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-14 10:43:05 +0300

    Input data through frontend

--- src/djangoapp/views.py
+++ src/djangoapp/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+from .forms import ComputerForm
 
 # Create your views here.
 def home(request):
-	title = 'Welcome: This is the Home Page'
-	context = {
-	"title": title,
-	}
-	return render(request, "home.html",context)
+    title = 'Welcome: This is the Home Page'
+    context = {
+        "title": title,
+    }
+    return render(request, "home.html", context)
+
+def computer_entry(request):
+    title = "Add Computer"
+    form = ComputerForm(request.POST or None)
+    
+    if form.is_valid():
+        form.save()
+    
+    context = {
+        "title": title,
+        "form": form,
+    }
+    
+    return render(request, "computer_entry.html", context)
