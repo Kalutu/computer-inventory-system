ce3425bced651c7170423f63e94fd1fdae1581f9
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-15 15:26:57 +0300

    Form Validation

--- src/djangoapp/forms.py
+++ src/djangoapp/forms.py
@@ -8,6 +8,25 @@ class ComputerForm(forms.ModelForm):
         widgets = {
             'purchase_date': forms.DateInput(attrs={'class': 'dateinput'}),
         }
+    def clean_computer_name(self):
+        computer_name = self.cleaned_data.get('computer_name')
+        if (computer_name == ""):
+            raise forms.ValidationError('This field cannot be left blank')
+        for instance in Computer.objects.all():
+            if instance.computer_name == computer_name:
+                raise forms.ValidationError(computer_name + ' is already added')
+        return computer_name
+
+    def clean_users_name(self):
+        users_name = self.cleaned_data.get('users_name')
+        if (users_name == ""):
+            raise forms.ValidationError('This field cannot be left blank')
+        for instance in Computer.objects.all():
+            if instance.users_name == users_name:
+                raise forms.ValidationError(users_name + ' is already added')
+        return users_name
+
+
 class ComputerSearchForm(forms.ModelForm):
     class Meta:
         model = Computer
