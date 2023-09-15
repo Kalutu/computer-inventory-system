defa87a00f96d55aff93f1f07c700838aff55997
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-15 10:55:58 +0300

    Added Jquery UI to Django

--- src/djangoapp/forms.py
+++ src/djangoapp/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import Computer
 
 class ComputerForm(forms.ModelForm):
-   class Meta:
-     model = Computer
-     fields = ['computer_name', 'IP_address', 'MAC_address', 'users_name', 'location', 'purchase_date']
+    class Meta:
+        model = Computer
+        fields = ['computer_name', 'IP_address', 'MAC_address', 'users_name', 'location', 'purchase_date']
+        widgets = {
+            'purchase_date': forms.DateInput(attrs={'class': 'dateinput'}),
+        }
