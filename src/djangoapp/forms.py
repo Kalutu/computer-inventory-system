3874c6f2c03fbe5384af5ce836d68bb9f82330d2
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-15 15:47:25 +0300

    Added choice field

--- src/djangoapp/forms.py
+++ src/djangoapp/forms.py
@@ -4,7 +4,7 @@ from .models import Computer
 class ComputerForm(forms.ModelForm):
     class Meta:
         model = Computer
-        fields = ['computer_name', 'IP_address', 'MAC_address', 'users_name', 'location', 'purchase_date']
+        fields = ['computer_name', 'IP_address', 'MAC_address', 'operating_system', 'users_name', 'location', 'purchase_date']
         widgets = {
             'purchase_date': forms.DateInput(attrs={'class': 'dateinput'}),
         }
--- src/djangoapp/models.py
+++ src/djangoapp/models.py
@@ -6,11 +6,19 @@ class Computer(models.Model):
     computer_name = models.CharField(max_length=30, blank=True, null=True)
     IP_address = models.CharField(max_length=30)
     MAC_address = models.CharField(max_length=30)
+    timestamp = models.DateField(auto_now_add = True, auto_now = False, blank=True)
+    os_choice = (
+            ('Mac OS', 'Mac OS'),
+            ('Windows 11', 'Windows 11'),
+            ('Windows 10', 'Windows 10'),
+            ('Windows 8', 'Windows 8'),
+            ('Linux', 'Linux'),
+        )
+    operating_system = models.CharField(max_length=30, blank=True, null=True, choices=os_choice)
+
     users_name = models.CharField(max_length=30, blank=True, null=True)
     location = models.CharField(max_length=30)
     purchase_date = models.DateField("Purchase mm-dd-yy",auto_now_add = False, auto_now = False, blank=True, null=True)
-    timestamp = models.DateField(auto_now_add = True, auto_now = False, blank=True)
-
-
+    
 def __unicode__(self):
     return self.users_name + self.IP_address
