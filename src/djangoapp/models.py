7882e7bead2421544dbfef96067e78295b0bfedd
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-15 14:38:22 +0300

    Update the model to allow blank and null from all the search fields

--- src/djangoapp/models.py
+++ src/djangoapp/models.py
@@ -3,10 +3,10 @@ from datetime import datetime, date
 
 # Create your models here.
 class Computer(models.Model):
-    computer_name = models.CharField(max_length=30)
+    computer_name = models.CharField(max_length=30, blank=True, null=True)
     IP_address = models.CharField(max_length=30)
     MAC_address = models.CharField(max_length=30)
-    users_name = models.CharField(max_length=30)
+    users_name = models.CharField(max_length=30, blank=True, null=True)
     location = models.CharField(max_length=30)
     purchase_date = models.DateField("Purchase mm-dd-yy",auto_now_add = False, auto_now = False, blank=True, null=True)
     timestamp = models.DateField(auto_now_add = True, auto_now = False, blank=True)
