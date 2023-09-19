692763d73ffc61c3e64a5a5b140a382ae0064c18
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-19 10:13:30 +0300

    Updated the edit functionality

--- src/djangoapp/forms.py
+++ src/djangoapp/forms.py
@@ -12,18 +12,25 @@ class ComputerForm(forms.ModelForm):
         computer_name = self.cleaned_data.get('computer_name')
         if (computer_name == ""):
             raise forms.ValidationError('This field cannot be left blank')
-        for instance in Computer.objects.all():
-            if instance.computer_name == computer_name:
+        instance = self.instance
+        existing_computers = Computer.objects.exclude(id=instance.id)
+        for computer in existing_computers:
+            if computer.computer_name == computer_name:
                 raise forms.ValidationError(computer_name + ' is already added')
+        
         return computer_name
 
     def clean_users_name(self):
         users_name = self.cleaned_data.get('users_name')
         if (users_name == ""):
             raise forms.ValidationError('This field cannot be left blank')
-        for instance in Computer.objects.all():
-            if instance.users_name == users_name:
+
+        instance = self.instance
+        existing_computers = Computer.objects.exclude(id=instance.id)
+        for computer in existing_computers:
+            if computer.users_name == users_name:
                 raise forms.ValidationError(users_name + ' is already added')
+        
         return users_name
 
 
