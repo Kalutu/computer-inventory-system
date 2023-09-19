9ca5ff9ffb8cc067a0df40654e73facee9e24710
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-19 10:35:20 +0300

    Coonect Django to mysql

--- src/djangoproject/settings.py
+++ src/djangoproject/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
