01111f0efae183a61db595c4a4e928afe4bf5a12
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-20 11:32:44 +0300

    modified:   src/djangoapp/views.py

--- src/djangoapp/views.py
+++ src/djangoapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import ComputerForm, ComputerSearchForm, OperatingSystemForm
-from .models import Computer
+from .models import Computer, ComputerHistory
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from django.contrib import messages
