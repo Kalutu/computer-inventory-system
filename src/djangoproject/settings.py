b86fb827482ce31a4cb61d8656c35671023bf218
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-21 15:55:22 +0300

    modified:   src/djangoproject/settings.py

--- src/djangoproject/settings.py
+++ src/djangoproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'computerinventory',
         'USER': 'Kalutu',
-        'PASSWORD': 'kalutuDan@2023',
+        'PASSWORD': os.environ['DBPASS'],
     }
 }
 
